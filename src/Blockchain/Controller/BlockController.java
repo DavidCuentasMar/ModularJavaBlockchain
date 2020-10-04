c79c29574ec8960b8893bf02efbfc34dd9d2e742
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-10-04 17:39:05 -0500

    Block & Transaction Models

---
+++ src/Blockchain/Controller/BlockController.java
@@ -0,0 +1,9 @@
+package Blockchain.Controller;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BlockController {
+
+}
---
+++ src/Blockchain/Model/Transaction.java
@@ -0,0 +1,8 @@
+package Blockchain.Model;
+public class Transaction {
+    String from_address;
+    String to_address;
+    String[] data;
+    String hash;
+    String signature;
+}
