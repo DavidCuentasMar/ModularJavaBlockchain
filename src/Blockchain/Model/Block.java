eaed4667105c2346120754de696e771d63573c6c
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-10-04 20:23:45 -0500

    Getters & Setters

--- src/Blockchain/Model/Block.java
+++ src/Blockchain/Model/Block.java
@@ -11,4 +11,79 @@ public class Block {
     String hash;
     int nonce;
     Random r;
+    
+    public Block(int index, LocalDateTime timestamp, ArrayList transactions, String previousHash, int difficulty) {
+        r = new Random();
+        this.index = index;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.nonce = r.nextInt();
+        //TO-DO hash
+    }
+
+    public int getIdCadena() {
+        return idCadena;
+    }
+
+    public void setIdCadena(int idCadena) {
+        this.idCadena = idCadena;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(ArrayList<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+
+    public Random getR() {
+        return r;
+    }
+
+    public void setR(Random r) {
+        this.r = r;
+    }
+    
 }
--- src/Blockchain/Model/Transaction.java
+++ src/Blockchain/Model/Transaction.java
@@ -5,4 +5,45 @@ public class Transaction {
     String[] data;
     String hash;
     String signature;
+
+    public String getFrom_address() {
+        return from_address;
+    }
+
+    public void setFrom_address(String from_address) {
+        this.from_address = from_address;
+    }
+
+    public String getTo_address() {
+        return to_address;
+    }
+
+    public void setTo_address(String to_address) {
+        this.to_address = to_address;
+    }
+
+    public String[] getData() {
+        return data;
+    }
+
+    public void setData(String[] data) {
+        this.data = data;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+    
 }
