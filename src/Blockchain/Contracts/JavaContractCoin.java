51c987bd2e4f37556b043492c7167deba756bd9c
dcuentas <dcuentas@grupoalto.com>
2020-11-22 22:18:16 -0500

    [feature: SmartContract -> JavaContractCoin]

--- src/Blockchain/Contracts/JavaContractCoin.java
+++ src/Blockchain/Contracts/JavaContractCoin.java
@@ -7,47 +7,74 @@ import java.util.ArrayList;
 
 public class JavaContractCoin extends JavaContract {
 
-    public boolean run(Chain theChain, Transaction tx, ArrayList<Transaction> txsToValidate) {
-        System.out.println("################");
-        System.out.println(txsToValidate.size());
-        System.out.println("################");
+    public boolean run(Chain theChain, Transaction tx, ArrayList<Transaction> correctUnpublishedTxs) {
+        System.out.println("[Start - Random Current Tx]");
+        System.out.println("    PublicKey: " + tx.from_address);
+        System.out.println("    SmartContract:" + tx.to_address);
+        System.out.println("    PubliKey Destiny: " + tx.getData()[0]);
+        System.out.println("    Amount: " + tx.getData()[1]);
+        System.out.println("[End - Random Current Tx]");
         boolean contractSuccess = false;
         double amountToSend = Double.valueOf(tx.getData()[1]);
-        double totalInput = 0;
-        double totalOutput = 0;
+        double totalInput = 0.0;
+        double totalOutput = 0.0;
+        if (correctUnpublishedTxs.size() > 0) {
+            for (Transaction currentCorrectUnpublishedTx : correctUnpublishedTxs) {
+                if ((currentCorrectUnpublishedTx.getFrom_address()).equals(tx.getFrom_address())) {
+                    totalOutput = totalOutput + Double.valueOf(currentCorrectUnpublishedTx.getData()[1]);
+                }
+                if ((currentCorrectUnpublishedTx.getData()[0]).equals(tx.getFrom_address())) {
+                    totalInput = totalInput + Double.valueOf(currentCorrectUnpublishedTx.getData()[1]);
+                }
+            }
+        }
         ArrayList<Transaction> targetTransactions = new ArrayList<>();
         for (Block currentBlock : theChain.getChain()) {
-//            System.out.println(currentBlock.getIndex());
-//            System.out.println(currentBlock.getTransactions().get(0).getFrom_address());
-//            System.out.println(currentBlock.getTransactions().get(0).getTo_address());
-//            System.out.println(currentBlock.getTransactions().get(0).getData()[0]);
-//            System.out.println(currentBlock.getTransactions().get(0).getData()[1]);
-
-            for (Transaction currentBlockTx : currentBlock.getTransactions()) {
-                System.out.println("currentBlockTx.getTo_address()");
-                System.out.println(currentBlockTx.getTo_address());
-                if (currentBlockTx.getTo_address() == "JavaContractCoin") {
-                    if (currentBlock.getIndex() == 0) {
-                        if (currentBlockTx.getData()[0] == tx.getFrom_address()) {
-                            totalInput += Double.valueOf(currentBlockTx.getData()[1]);
-                        }
-                    } else {
-                        if (currentBlockTx.getFrom_address() == tx.getFrom_address()) {
-                            totalOutput += Double.valueOf(currentBlockTx.getData()[1]);
-                        }
-                        if (currentBlockTx.getData()[0] == tx.getFrom_address()) {
-                            totalInput += Double.valueOf(currentBlockTx.getData()[1]);
+            if (currentBlock.getIndex() == 0) {
+                System.out.println("[Checking Genesis] " + theChain.getChainSize());
+                for (Transaction currentBlockTx : currentBlock.getTransactions()) {
+                    if (currentBlockTx.getTo_address() == "JavaContractCoin") {
+                        System.out.println("----");
+                        System.out.println(currentBlockTx.getData()[0]);
+                        System.out.println(tx.getFrom_address());
+                        System.out.println("----");
+                        System.out.println(totalInput);
+                        System.out.println(Double.valueOf(currentBlockTx.getData()[1]));
+                        System.out.println(totalInput + Double.valueOf(currentBlockTx.getData()[1]));
+                        if ((currentBlockTx.getData()[0]).equals(tx.getFrom_address())) {
+                            totalInput = totalInput + Double.valueOf(currentBlockTx.getData()[1]);
                         }
                     }
                 }
+            } else {
+                System.out.println("{-----NO GENESIS - BLOCK # " + currentBlock.getIndex() + "}----");
+                System.out.println(currentBlock.getTransactions().size());
+                for (Transaction currentBlockTx : currentBlock.getTransactions()) {
+                    System.out.println("SmartConract: " + currentBlockTx.getTo_address());
+                    if ((currentBlockTx.getTo_address()).equals("JavaContractCoin")) {
+                        System.out.println("ORIGIN: " + currentBlockTx.getFrom_address());
+                        System.out.println("DESTINY: " + tx.getFrom_address());
+                        if ((currentBlockTx.getData()[0]).equals(tx.getFrom_address())) {
+                            totalInput = totalInput + Double.valueOf(currentBlockTx.getData()[1]);
+                        }
+                        if ((currentBlockTx.getFrom_address()).equals(tx.getFrom_address())) {
+                            totalOutput = totalOutput + Double.valueOf(currentBlockTx.getData()[1]);
+                        }
+                    }
+                }
+                System.out.println("{-----END NO GENESIS}----");
+
             }
         }
         if ((totalInput - totalOutput) >= amountToSend) {
             contractSuccess = true;
         }
-        System.out.println(totalInput);
-        System.out.println(totalOutput);
-        System.out.println("*****");
+        System.out.println("totalInput: " + totalInput);
+        System.out.println("totalOutput: " + totalOutput);
+        System.out.println("amountToSend: " + amountToSend);
+        System.out.println("################");
+        System.out.println(correctUnpublishedTxs.size());
+        System.out.println("################");
         return contractSuccess;
     }
 
